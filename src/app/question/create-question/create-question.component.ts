7864094a913e79f1ea08f9a374da9d0abdc0005e
Yago César <yagocdj@gmail.com>
2025-01-10 17:14:16 -0300

    feat: add fields to select question's subject and difficulty

--- src/app/question/create-question/create-question.component.ts
+++ src/app/question/create-question/create-question.component.ts
@@ -1,20 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
+import { PanelModule } from 'primeng/panel';
 import { TextareaModule } from 'primeng/textarea';
 import { IftaLabelModule } from 'primeng/iftalabel';
+import { SelectModule } from 'primeng/select';
+import { Subject } from '../../shared/models/subject';
+import { Difficulty } from '../../shared/models/difficulty';
 
 @Component({
   selector: 'app-create-question',
-  imports: [CardModule, ButtonModule, TextareaModule, ReactiveFormsModule, IftaLabelModule],
+  imports: [PanelModule, ButtonModule, TextareaModule, ReactiveFormsModule, IftaLabelModule,
+    SelectModule,
+  ],
   templateUrl: './create-question.component.html',
   styleUrl: './create-question.component.css'
 })
-export class CreateQuestionComponent {
+export class CreateQuestionComponent implements OnInit {
+  
   readonly maxChars = 200;
+  subjects: Subject[] = [];
+  difficulties: Difficulty[] = [];
+
+  ngOnInit() {
+    this.subjects = [
+      { id: 1, name: 'Matemática' },
+      { id: 2, name: 'Português' },
+      { id: 3, name: 'Raciocínio Lógico' },
+      { id: 4, name: 'Direito Administrativo' },
+    ];
+
+    this.difficulties = [
+      { id: 1, name: 'Fácil' },
+      { id: 2, name: 'Média' },
+      { id: 3, name: 'Difícil' },
+    ]
+  }
 
   questionCreationForm = new FormGroup({
     questionText: new FormControl('', Validators.required),
+    subject: new FormControl(new Subject(''), Validators.required),
+    difficulty: new FormControl(new Difficulty(''), Validators.required),
   });
 }
