1caf3c0bda42dd614f0e629525978dd7825b2f7f
Yago César <yagocdj@gmail.com>
2025-01-23 20:00:12 -0300

    feat: use index to track for

--- src/app/question/create-question/option-select/option-select.component.ts
+++ src/app/question/create-question/option-select/option-select.component.ts
@@ -1,34 +1,40 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-option-select',
-  imports: [FormsModule],
+  imports: [FormsModule, ButtonModule,],
   templateUrl: './option-select.component.html',
   styleUrl: './option-select.component.css'
 })
 export class OptionSelectComponent {
   alternatives = [
-    { id: 1, text: '' },
-    { id: 2, text: '' },
-    { id: 3, text: '' },
-    { id: 4, text: '' }
+    { text: '' },
+    { text: '' },
+    { text: '' },
+    { text: '' }
   ];
   selectedCorrectAlternative: number | null = null;
 
   addAlternative(): void {
-    this.alternatives.push({ id: this.alternatives.at(-1)!.id, text: '' });
+    this.alternatives.push({ text: '' });
   }
 
   removeAlternative(index: number): void {
-    if (this.alternatives.length > 2) {
-      this.alternatives.splice(index, 1);
-      // Adjust the correct alternative if it has been removed
-      if (this.selectedCorrectAlternative === index) {
-        this.selectedCorrectAlternative = null;
-      } else if (this.selectedCorrectAlternative !== null && this.selectedCorrectAlternative > index) {
-        this.selectedCorrectAlternative--;
-      }
+    // Ensure that there are always at least two alternatives
+    if (this.alternatives.length < 2) {
+      return;
+    }
+
+    // index is 1-based, so we need to subtract 1 to get the correct index
+    this.alternatives.splice(index - 1, 1);
+
+    // Adjust the correct alternative if it has been removed
+    if (this.selectedCorrectAlternative === index) {
+      this.selectedCorrectAlternative = null;
+    } else if (this.selectedCorrectAlternative !== null && this.selectedCorrectAlternative > index) {
+      this.selectedCorrectAlternative--;
     }
   }
 }
