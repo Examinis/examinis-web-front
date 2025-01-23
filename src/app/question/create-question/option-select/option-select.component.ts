74582a3dbb432a013614548b22b799949ad1fd60
Yago César <yagocdj@gmail.com>
2025-01-23 19:23:54 -0300

    feat: add fields for option creation

--- src/app/question/create-question/option-select/option-select.component.ts
+++ src/app/question/create-question/option-select/option-select.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-option-select',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './option-select.component.html',
   styleUrl: './option-select.component.css'
 })
 export class OptionSelectComponent {
+  alternatives = [
+    { id: 1, text: '' },
+    { id: 2, text: '' },
+    { id: 3, text: '' },
+    { id: 4, text: '' }
+  ];
+  selectedCorrectAlternative: number | null = null;
 
+  addAlternative(): void {
+    this.alternatives.push({ id: this.alternatives.at(-1)!.id, text: '' });
+  }
+
+  removeAlternative(index: number): void {
+    if (this.alternatives.length > 2) {
+      this.alternatives.splice(index, 1);
+      // Adjust the correct alternative if it has been removed
+      if (this.selectedCorrectAlternative === index) {
+        this.selectedCorrectAlternative = null;
+      } else if (this.selectedCorrectAlternative !== null && this.selectedCorrectAlternative > index) {
+        this.selectedCorrectAlternative--;
+      }
+    }
+  }
 }
--- src/app/shared/models/option.ts
+++ src/app/shared/models/option.ts
@@ -1,2 +1,15 @@
+import { Question } from "./question";
+
 export class Option {
+  id: number;
+  question: Question;
+  description: string;
+  isCorrect: boolean;
+
+  constructor(id: number, question: Question, description: string, isCorrect: boolean) {
+    this.id = id;
+    this.question = question;
+    this.description = description;
+    this.isCorrect = isCorrect;
+  }
 }
