1e9bbff91e24ce55d79a2c96b1aa3d795bff4b64
Yago César <yagocdj@gmail.com>
2025-01-24 13:42:04 -0300

    feat: improve options input

--- src/app/question/create-question/option-select/option-select.component.ts
+++ src/app/question/create-question/option-select/option-select.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 
@@ -9,32 +9,40 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './option-select.component.css'
 })
 export class OptionSelectComponent {
-  alternatives = [
+
+  @Output() selectedOptionEvent = new EventEmitter<Object>();
+
+  options = [
     { text: '' },
     { text: '' },
     { text: '' },
     { text: '' }
   ];
-  selectedCorrectAlternative: number | null = null;
+  selectedCorrectOption: number | null = null;
 
-  addAlternative(): void {
-    this.alternatives.push({ text: '' });
+  addOption(): void {
+    if (this.options.length < 5) {
+      this.options.push({ text: '' });
+    }
   }
 
-  removeAlternative(index: number): void {
-    // Ensure that there are always at least two alternatives
-    if (this.alternatives.length < 2) {
+  chooseCorrectOption(index: number): void {
+    this.selectedOptionEvent.emit(this.options[index]);
+  }
+
+  removeOption(index: number): void {
+    // Ensure that there are always at least two options
+    if (this.options.length < 2) {
       return;
     }
 
-    // index is 1-based, so we need to subtract 1 to get the correct index
-    this.alternatives.splice(index - 1, 1);
+    this.options.splice(index, 1);
 
     // Adjust the correct alternative if it has been removed
-    if (this.selectedCorrectAlternative === index) {
-      this.selectedCorrectAlternative = null;
-    } else if (this.selectedCorrectAlternative !== null && this.selectedCorrectAlternative > index) {
-      this.selectedCorrectAlternative--;
+    if (this.selectedCorrectOption === index) {
+      this.selectedCorrectOption = null;
+    } else if (this.selectedCorrectOption !== null && this.selectedCorrectOption > index) {
+      this.selectedCorrectOption--;
     }
   }
 }
--- src/app/shared/models/option.ts
+++ src/app/shared/models/option.ts
@@ -4,12 +4,14 @@ export class Option {
   id: number;
   question: Question;
   description: string;
+  letter: string;
   isCorrect: boolean;
 
-  constructor(id: number, question: Question, description: string, isCorrect: boolean) {
+  constructor(id: number, question: Question, description: string, isCorrect: boolean, letter: string) {
     this.id = id;
     this.question = question;
     this.description = description;
     this.isCorrect = isCorrect;
+    this.letter = letter;
   }
 }
