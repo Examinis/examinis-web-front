777bd1b3ba1c76ec89c291e55825b5bcfcb55e5c
Yago César <yagocdj@gmail.com>
2025-01-27 14:17:01 -0300

    feat: add environments (development and production)

--- src/app/shared/services/question-api.service.ts
+++ src/app/shared/services/question-api.service.ts
@@ -2,19 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Question } from '../interfaces/question';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionApiService {
- 
+
   private http: HttpClient = inject(HttpClient);
-  private readonly BASE_URL = 'http://localhost:8000/api' ;  // TODO : Add the URL to an environment variable
 
   constructor() { }
 
   getQuestions(): Observable<Question> {
-    return this.http.get<Question>(this.BASE_URL + '/question');
+    return this.http.get<Question>(environment.apiUrl + '/question');
   }
 
 }
