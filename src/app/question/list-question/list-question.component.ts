530724d4b664bb57980d87d27de937a38f27560e
paulosys <paulosilvasergio486@gmail.com>
2025-01-30 23:02:24 -0300

    refactor

--- src/app/question/list-question/list-question.component.ts
+++ src/app/question/list-question/list-question.component.ts
@@ -1,28 +1,35 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QuestionApiService } from '../../shared/services/question-api.service';
-import { Question } from '../../shared/interfaces/question';
 import { FormsModule } from '@angular/forms';
-import { FilterCategoryPipe } from '../../shared/pipes/filter-category.pipe';
-import { FilterDifficultyPipe } from '../../shared/pipes/filter-difficulty.pipe';
-import { Subject } from '../../shared/interfaces/subject';
-import { Difficulty } from '../../shared/interfaces/difficulty';
-import { SubjectApiService } from '../../shared/services/subject-api.service';
 import { catchError, forkJoin, of } from 'rxjs';
-import { Page } from '../../shared/interfaces/page';
+
 import { TableModule } from 'primeng/table';
 import { PaginatorModule } from 'primeng/paginator';
 import { CardModule } from 'primeng/card';
 import { DropdownModule } from 'primeng/dropdown';
-import { ConfirmDialog } from 'primeng/confirmdialog';
-import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ToastModule } from 'primeng/toast';
 import { ButtonModule } from 'primeng/button';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
+import { QuestionApiService } from '../../shared/services/question-api.service';
+import { SubjectApiService } from '../../shared/services/subject-api.service';
+
+import { Question } from '../../shared/interfaces/question';
+import { Subject } from '../../shared/interfaces/subject';
+import { Difficulty } from '../../shared/interfaces/difficulty';
+import { Page } from '../../shared/interfaces/page';
+
+import { FilterCategoryPipe } from '../../shared/pipes/filter-category.pipe';
+import { FilterDifficultyPipe } from '../../shared/pipes/filter-difficulty.pipe';
+
 
 @Component({
   selector: 'app-question-list',
   templateUrl: './list-question.component.html',
+  styleUrls: ['./list-question.component.css'],
   providers: [ConfirmationService, MessageService],
+  standalone: true,
   imports: [
     CommonModule,
     FormsModule,
@@ -30,58 +37,54 @@ import { ButtonModule } from 'primeng/button';
     TableModule,
     PaginatorModule,
     DropdownModule,
-    ConfirmDialog,
+    ConfirmDialogModule,
     ButtonModule,
     ToastModule,
-    FilterCategoryPipe, FilterDifficultyPipe],
-  styleUrls: ['./list-question.component.css']
+    FilterCategoryPipe,
+    FilterDifficultyPipe
+  ],
 })
 export class QuestionListComponent implements OnInit {
+  private questionApiService = inject(QuestionApiService);
+  private subjectApiService = inject(SubjectApiService);
 
-  questions: Page<Question> = {
-    total: 0,
-    page: 1,
-    size: 10,
-    results: []
-  };
+  questions: Page<Question> = { total: 0, page: 1, size: 10, results: [] };
   filteredQuestions: Question[] = [];
 
   categories: Subject[] = [];
   selectedSubject?: Subject;
-  selectedDifficulty?: Difficulty;
+
   difficulties: Difficulty[] = [
     { id: 1, name: 'Fácil' },
     { id: 2, name: 'Médio' },
-    { id: 3, name: 'Difícil' }
-  ]
+    { id: 3, name: 'Difícil' },
+  ];
+  selectedDifficulty?: Difficulty;
 
-  private questionApiService: QuestionApiService = inject(QuestionApiService);
-  private subjectApiService: SubjectApiService = inject(SubjectApiService);
-
-  constructor(private confirmationService: ConfirmationService,
-    private messageService: MessageService) { }
+  constructor(
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
-    this.loadData(1, this.questions.size);
+    this.loadData();
   }
 
-  loadData(page = 1, size = this.questions.size): void {
+  loadData(page: number = 1, size: number = this.questions.size): void {
     forkJoin({
-      subjects: this.subjectApiService.getSubjects().pipe(catchError(error => {
-        console.error('Error fetching subjects', error);
-        return of([]);
-      })),
-      questions: this.questionApiService.getQuestions(page, size).pipe(catchError(error => {
-        console.error('Error fetching questions', error);
-        return of({ total: 0, page: 1, size: 10, results: [] });
-      }))
+      subjects: this.subjectApiService.getSubjects().pipe(
+        catchError(() => of([]))
+      ),
+      questions: this.questionApiService.getQuestions(page, size).pipe(
+        catchError(() => of({ total: 0, page: 1, size: 10, results: [] }))
+      ),
     }).subscribe({
       next: ({ subjects, questions }) => {
         this.categories = subjects;
         this.questions = questions;
         this.applyFilters();
       },
-      error: error => console.error('Error in forkJoin', error)
+      error: (error) => console.error('Error in forkJoin', error),
     });
   }
 
@@ -89,8 +92,9 @@ export class QuestionListComponent implements OnInit {
     this.applyFilters();
   }
 
+
   applyFilters(): void {
-    this.filteredQuestions = this.questions.results.filter(question => {
+    this.filteredQuestions = this.questions.results.filter((question) => {
       const subjectMatch = !this.selectedSubject || (question.subject && question.subject.id === this.selectedSubject.id);
       const difficultyMatch = !this.selectedDifficulty || (question.difficulty && question.difficulty.id === this.selectedDifficulty.id);
       return subjectMatch && difficultyMatch;
@@ -98,12 +102,8 @@ export class QuestionListComponent implements OnInit {
   }
 
   onPageChange(event: any): void {
-    const page = event.first || 0;
-    this.questions.page = Math.floor(page / this.questions.size) + 1;
-    this.questions.size = event.rows;
-    this.loadData(this.questions.page, this.questions.size);
+    this.loadData(event.page + 1, event.rows);
   }
-
 
   viewQuestion(question: Question): void {
     console.log('Visualizando questão:', question);
@@ -113,7 +113,7 @@ export class QuestionListComponent implements OnInit {
     console.log('Editando questão:', question);
   }
 
-  deleteQuestion(event: Event) {
+  deleteQuestion(event: Event): void {
     this.confirmationService.confirm({
       target: event.target as EventTarget,
       message: 'Tem certeza de que deseja excluir esta questão?',
@@ -129,27 +129,15 @@ export class QuestionListComponent implements OnInit {
         severity: 'danger',
       },
       accept: () => {
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Exclusão bem-sucedida',
-          detail: 'A questão foi excluída com sucesso.'
-        });
+        this.messageService.add({ severity: 'success', summary: 'Excluído', detail: 'Questão excluída' });
       },
       reject: () => {
-        this.messageService.add({
-          severity: 'info',
-          summary: 'Exclusão cancelada',
-          detail: 'A operação de exclusão foi cancelada, a questão permanece no sistema.'
-        });
+        this.messageService.add({ severity: 'info', summary: 'Cancelado', detail: 'Exclusão cancelada' });
       },
     });
   }
 
   toggleDifficulty(difficulty: Difficulty): void {
-    if (this.selectedDifficulty === difficulty) {
-      this.selectedDifficulty = undefined;
-    } else {
-      this.selectedDifficulty = difficulty;
-    }
+    this.selectedDifficulty = this.selectedDifficulty === difficulty ? undefined : difficulty;
   }
 }
