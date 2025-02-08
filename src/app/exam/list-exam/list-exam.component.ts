9e4dbb530ee92fd6a9e97b05a34318193cfcc781
Yago César <yagocdj@gmail.com>
2025-02-08 15:49:21 -0300

    feat: update list exam component to support pagination and refactor exam data structure

--- src/app/exam/list-exam/list-exam.component.ts
+++ src/app/exam/list-exam/list-exam.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
@@ -7,6 +7,7 @@ import { PaginatorModule } from 'primeng/paginator';
 import { ToastModule } from 'primeng/toast';
 import { SidebarDrawerComponent } from "../../shared/components/sidebar-drawer/sidebar-drawer.component";
 import { Exam } from '../../shared/interfaces/exam';
+import { Page } from '../../shared/interfaces/page';
 
 @Component({
   selector: 'app-list-exam',
@@ -18,21 +19,18 @@ import { Exam } from '../../shared/interfaces/exam';
   providers: [MessageService, ConfirmationService]
 })
 export class ListExamComponent {
-  exams: Exam[] = []; // Substitua por seus dados de provas
-  totalRecords: number = 0;
+  private router: Router = inject(Router);
 
+  exams: Page<Exam> = { total: 0, page: 1, size: 10, results: [] };
+  filteredExams: Exam[] = [];
 
-  // constructor(private confirmationService: ConfirmationService, private router: Router) {}
-  constructor(private router: Router) {}
+  // constructor(private confirmationService: ConfirmationService) {}
+  constructor() {}
 
   ngOnInit() {
     // Carregue os dados das provas aqui, por exemplo, de um serviço
     // this.examService.getExams().subscribe(exams => this.exams = exams);
     this.exams = this.mockExams();
-  }
-
-  trackByExamId(index: number, exam: Exam): number {
-    return exam.id;
   }
 
   viewExam(examId: number) {
@@ -67,79 +65,16 @@ export class ListExamComponent {
     // Atualize a lista de exames com base nos parâmetros de paginação
   }
 
-  private mockExams(): Exam[] {
-    return [
-      {
-        id: 1,
-        title: 'Prova 1',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        userId: 1
-      },
-      {
-        id: 2,
-        title: 'Prova 2',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        userId: 1
-      },
-      {
-        id: 3,
-        title: 'Prova 3',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        userId: 1
-      },
-      {
-        id: 4,
-        title: 'Prova 4',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        userId: 1
-      },
-      {
-        id: 5,
-        title: 'Prova 5',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        userId: 1
-      },
-      {
-        id: 6,
-        title: 'Prova 6',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        userId: 1
-      },
-      {
-        id: 7,
-        title: 'Prova 7',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        userId: 1
-      },
-      {
-        id: 8,
-        title: 'Prova 8',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        userId: 1
-      },
-      {
-        id: 9,
-        title: 'Prova 9',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        userId: 1
-      },
-      {
-        id: 10,
-        title: 'Prova 10',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        userId: 1
-      }
-    ];
+  private mockExams(): Page<Exam> {
+    return {
+      total: 2,
+      page: 1,
+      size: 10,
+      results: [
+        { id: 1, title: 'Prova 1', createdAt: new Date(), updatedAt: new Date(), userId: 1 },
+        { id: 2, title: 'Prova 2', createdAt: new Date(), updatedAt: new Date(), userId: 1 },
+      ]
+    };
   }
 
 }
