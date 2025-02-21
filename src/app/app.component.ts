9962648b793f4d9663af01420018fc5193da8593
paulosys <paulosilvasergio486@gmail.com>
2025-02-20 21:11:38 -0300

    feat: conditionally render sidebar based on home route and update layout styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   
 })
-export class AppComponent { 
+export class AppComponent implements OnInit { 
+  isHomeRoute: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      this.isHomeRoute = this.router.url === '/';  // Verifique a rota
+    });
+  }
 }
